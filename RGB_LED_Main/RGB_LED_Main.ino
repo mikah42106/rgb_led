04178969cc6d938d667b52154b0d70e8299c4790
geoffberl <gberl001@gmail.com>
2016-12-03 13:43:45 -0500

    Added optional parameters for delay and brightness

--- RGB_LED_Main/RGB_LED_Main.ino
+++ RGB_LED_Main/RGB_LED_Main.ino
@@ -9,6 +9,12 @@
 
 #define FADESPEED 100     // make this higher to slow down
 
+// Function declarations
+void colorFade(byte fromColor, byte toColor, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255);
+void fadeOut(byte color, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255);
+void fadeIn(byte color, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255);
+
+
 void setup() {
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
@@ -18,74 +24,73 @@ void setup() {
 
 
 void loop() {
-  int r, g, b;
-  int w = 150;
-  int q = 0;
-  int poo = 1;
-  //fade from red to green
-  //for (r = 0; r <= MAX_BRIGHTNESS; r++) {
-  //analogWrite(REDPIN, r);
-  //delay(FADESPEED);
-
-  // fade from blue to violet
-      for (r = 0; r <= MAX_BRIGHTNESS; r++) {
-        analogWrite(REDPIN, r);
-        delay(FADESPEED);
-      }
-  // fade from violet to red
-  for (b = MAX_BRIGHTNESS; b > 0; b--) {
-  analogWrite(BLUEPIN, b);
-  delay(FADESPEED);
-  }
-  // fade from red to yellow
-  for (g = 0; g <= MAX_BRIGHTNESS; g++) {
-  analogWrite(GREENPIN, g);
-  delay(FADESPEED);
-  }
-  // fade from yellow to green
-  for (r = MAX_BRIGHTNESS; r > 0; r--) {
-  analogWrite(REDPIN, r);
-  delay(FADESPEED);
-  }
-  // fade from green to teal
-  for (b = 0; b <= MAX_BRIGHTNESS; b++) {
-  analogWrite(BLUEPIN, b);
-  delay(FADESPEED);
-  }
-  // fade from teal to blue
-  for (g = MAX_BRIGHTNESS; g > 0; g--) {
-  analogWrite(GREENPIN, g);
-  delay(FADESPEED);
-      colorFade(REDPIN, GREENPIN);
-      colorFade(GREENPIN, REDPIN);
-      fadeOut(REDPIN);
-      fadeIn(GREENPIN);
-      fadeOut(GREENPIN);
-      fadeIn(REDPIN);
+//  int r, g, b;
+//  int w = 150;
+//  int q = 0;
+//  int poo = 1;
+//  //fade from red to green
+//  //for (r = 0; r <= MAX_BRIGHTNESS; r++) {
+//  //analogWrite(REDPIN, r);
+//  //delay(FADESPEED);
+//
+//  // fade from blue to violet
+//      for (r = 0; r <= MAX_BRIGHTNESS; r++) {
+//        analogWrite(REDPIN, r);
+//        delay(FADESPEED);
+//      }
+//  // fade from violet to red
+//  for (b = MAX_BRIGHTNESS; b > 0; b--) {
+//  analogWrite(BLUEPIN, b);
+//  delay(FADESPEED);
+//  }
+//  // fade from red to yellow
+//  for (g = 0; g <= MAX_BRIGHTNESS; g++) {
+//  analogWrite(GREENPIN, g);
+//  delay(FADESPEED);
+//  }
+//  // fade from yellow to green
+//  for (r = MAX_BRIGHTNESS; r > 0; r--) {
+//  analogWrite(REDPIN, r);
+//  delay(FADESPEED);
+//  }
+//  // fade from green to teal
+//  for (b = 0; b <= MAX_BRIGHTNESS; b++) {
+//  analogWrite(BLUEPIN, b);
+//  delay(FADESPEED);
+//  }
+//  // fade from teal to blue
+//  for (g = MAX_BRIGHTNESS; g > 0; g--) {
+//  analogWrite(GREENPIN, g);
+//  delay(FADESPEED);
+//      colorFade(REDPIN, GREENPIN);
+//      colorFade(GREENPIN, REDPIN);
+      fadeOut(REDPIN, 100, 0, 25);
+      fadeIn(GREENPIN, 5, 0, 25);
+      fadeOut(GREENPIN, 5, 0, 25);
+      fadeIn(REDPIN, 100, 0, 25);
   
+//}
 }
-}
-void colorFade(byte fromColor, byte toColor) {
-  int i = 0;
-  for (i = 0; i <= MAX_BRIGHTNESS; i++) {
-    analogWrite(fromColor,  MAX_BRIGHTNESS - i);
-    delay(FADESPEED);
+
+void colorFade(byte fromColor, byte toColor, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255) {
+  for (int i = minBrightness; i <= maxBrightness; i++) {
+    analogWrite(fromColor,  maxBrightness - i);
     analogWrite(toColor, i);
-    delay(FADESPEED);
+    delay(delayMS);
   }
 }
 
-void fadeOut(byte color) {
-  for (int out = MAX_BRIGHTNESS; out > 0; out--) {
+void fadeOut(byte color, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255) {
+  for (int out = maxBrightness; out > minBrightness; out--) {
     analogWrite(color, out);
-    delay(FADESPEED);
+    delay(delayMS);
   }
 }
 
-void fadeIn(byte color) {
-  for (int in = 0; in <= MAX_BRIGHTNESS; in++) {
+void fadeIn(byte color, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255) {
+  for (int in = minBrightness; in <= maxBrightness; in++) {
     analogWrite(color, in);
-    delay(FADESPEED);
+    delay(delayMS);
   }
 }
 
