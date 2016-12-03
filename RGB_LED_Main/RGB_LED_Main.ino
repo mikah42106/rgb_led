10b8112e99b1dc642050bcb87df47191d63a18c6
geoffberl <gberl001@gmail.com>
2016-12-02 21:48:46 -0500

    Added source code

--- RGB_LED_Main/RGB_LED_Main.ino
+++ RGB_LED_Main/RGB_LED_Main.ino
@@ -1,9 +1,52 @@
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+// public domain, enjoy!
+ 
+#define REDPIN 9
+#define GREENPIN 10
+#define BLUEPIN 11
+#define MAX_BRIGHTNESS 25
+ 
+#define FADESPEED 100     // make this higher to slow down
+ 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
 }
-
+ 
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int r, g, b;
+ 
+  // fade from blue to violet
+  for (r = 0; r <= MAX_BRIGHTNESS; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = MAX_BRIGHTNESS; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g <= MAX_BRIGHTNESS; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = MAX_BRIGHTNESS; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b <= MAX_BRIGHTNESS; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = MAX_BRIGHTNESS; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
 }
