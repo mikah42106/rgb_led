9a95b266954eb8cd83be6d83fb98923cb65ecb7e
geoffberl <gberl001@gmail.com>
2016-12-03 14:22:36 -0500

    Added comments

--- RGB_LED_Main/RGB_LED_Main.ino
+++ RGB_LED_Main/RGB_LED_Main.ino
@@ -25,6 +25,17 @@ void loop() {
   fadeIn(RED_PIN, 2000, 0, 25);
 }
 
+/*
+ * colorFade
+ * Fade from one primary color to another. You may optionally set the duration 
+ * which is defaulted to one second and you can optionally set the min and max 
+ * brightness values.
+ * @param fromColor the pin for the color you are changing from
+ * @param toColor the pin for the color you are changing to
+ * @param durationMS  the amount of time (in milliseconds) you want the fade to take
+ * @param minBrightness the lowest brightness value for the colors
+ * @param maxBrightness the highest brightness value for the colors
+ */
 void colorFade(byte fromColor, byte toColor, long durationMS = 1000, byte minBrightness = 0, byte maxBrightness = 255) {
   // Calculate the delay time increment required to satisfy the total duration desired
   long delayIncrement = durationMS / (maxBrightness - minBrightness);
@@ -35,6 +46,15 @@ void colorFade(byte fromColor, byte toColor, long durationMS = 1000, byte minBri
   }
 }
 
+/*
+ * fadeOut
+ * Fade a color out completely. You may optionally set the duration which is defaulted
+ * to one second and you can optionally set the min and max brightness values.
+ * @param color the pin for the color you are fading out
+ * @param durationMS  the amount of time (in milliseconds) you want the fade to take
+ * @param minBrightness the lowest brightness value for the colors
+ * @param maxBrightness the highest brightness value for the colors
+ */
 void fadeOut(byte color, long durationMS = 1000, byte minBrightness = 0, byte maxBrightness = 255) {
   // Calculate the delay time increment required to satisfy the total duration desired
   long delayIncrement = durationMS / (maxBrightness - minBrightness);
@@ -44,6 +64,15 @@ void fadeOut(byte color, long durationMS = 1000, byte minBrightness = 0, byte ma
   }
 }
 
+/*
+ * fadeIn
+ * Fade a color in completely. You may optionally set the duration which is defaulted
+ * to one second and you can optionally set the min and max brightness values.
+ * @param color the pin for the color you are fading in
+ * @param durationMS  the amount of time (in milliseconds) you want the fade to take
+ * @param minBrightness the lowest brightness value for the colors
+ * @param maxBrightness the highest brightness value for the colors
+ */
 void fadeIn(byte color, long durationMS = 1000, byte minBrightness = 0, byte maxBrightness = 255) {
   // Calculate the delay time increment required to satisfy the total duration desired
   long delayIncrement = durationMS / (maxBrightness - minBrightness);
