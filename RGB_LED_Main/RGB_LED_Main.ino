5eb60348cca7370affb4729bb61cf90df2cecccd
geoffberl <gberl001@gmail.com>
2016-12-03 13:52:35 -0500

    Cleaned up commented code

--- RGB_LED_Main/RGB_LED_Main.ino
+++ RGB_LED_Main/RGB_LED_Main.ino
@@ -19,57 +19,17 @@ void setup() {
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
-  
+
 }
 
 
 void loop() {
-//  int r, g, b;
-//  int w = 150;
-//  int q = 0;
-//  int poo = 1;
-//  //fade from red to green
-//  //for (r = 0; r <= MAX_BRIGHTNESS; r++) {
-//  //analogWrite(REDPIN, r);
-//  //delay(FADESPEED);
-//
-//  // fade from blue to violet
-//      for (r = 0; r <= MAX_BRIGHTNESS; r++) {
-//        analogWrite(REDPIN, r);
-//        delay(FADESPEED);
-//      }
-//  // fade from violet to red
-//  for (b = MAX_BRIGHTNESS; b > 0; b--) {
-//  analogWrite(BLUEPIN, b);
-//  delay(FADESPEED);
-//  }
-//  // fade from red to yellow
-//  for (g = 0; g <= MAX_BRIGHTNESS; g++) {
-//  analogWrite(GREENPIN, g);
-//  delay(FADESPEED);
-//  }
-//  // fade from yellow to green
-//  for (r = MAX_BRIGHTNESS; r > 0; r--) {
-//  analogWrite(REDPIN, r);
-//  delay(FADESPEED);
-//  }
-//  // fade from green to teal
-//  for (b = 0; b <= MAX_BRIGHTNESS; b++) {
-//  analogWrite(BLUEPIN, b);
-//  delay(FADESPEED);
-//  }
-//  // fade from teal to blue
-//  for (g = MAX_BRIGHTNESS; g > 0; g--) {
-//  analogWrite(GREENPIN, g);
-//  delay(FADESPEED);
-//      colorFade(REDPIN, GREENPIN);
-//      colorFade(GREENPIN, REDPIN);
-      fadeOut(REDPIN, 100, 0, 25);
-      fadeIn(GREENPIN, 5, 0, 25);
-      fadeOut(GREENPIN, 5, 0, 25);
-      fadeIn(REDPIN, 100, 0, 25);
-  
-//}
+  //  colorFade(REDPIN, GREENPIN);
+  //  colorFade(GREENPIN, REDPIN);
+  fadeOut(REDPIN, 100, 0, 25);
+  fadeIn(GREENPIN, 5, 0, 25);
+  fadeOut(GREENPIN, 5, 0, 25);
+  fadeIn(REDPIN, 100, 0, 25);
 }
 
 void colorFade(byte fromColor, byte toColor, byte delayMS = FADESPEED, byte minBrightness = 0, byte maxBrightness = 255) {
